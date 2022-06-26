96539c605afe6d0a8480f6a9f5f0d5a60d2d8e0a
Your Name <you@example.com>
2022-06-26 11:01:00 +0100

    yo2

---
+++ crash_course_book/chapter4/4_13_menu_tuples.py
@@ -0,0 +1,12 @@
+menu_items = (
+    'rockfish sandwich', 'halibut nuggets', 'smoked salmon chowder',
+    'salmon burger', 'crab cakes',
+    )
+#print(type(menu_items))
+for items in menu_items:
+    print(f"- {items}")
+print("\n There was a change in the menu")
+menu_items = ('pizza', 'rocket salad', 'sambo', 'cod & fries', 'tacos')
+for items in menu_items:
+    print(f"- {items}")
+
