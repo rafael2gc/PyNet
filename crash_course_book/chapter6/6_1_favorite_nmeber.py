1fa1b1357021f14ecf38035290acf995892d3929
Your Name <you@example.com>
2022-07-30 22:22:06 +0100

    fixes

--- crash_course_book/chapter6/6_1_favorite_nmeber.py
+++
@@ -1,5 +0,0 @@
-person = {'first_name': 'Rafael', 'last_name': 'Costa', 'age': '44', 'city': 'Dublin'}
-print(person['first_name'])
-print(person['last_name'])
-print(person['age'])
-print(person['city'])
