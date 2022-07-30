82373f396e103e74bc2b9ce5897ac9bf0bb97713
Your Name <you@example.com>
2022-07-30 22:20:04 +0100

    chapter 6

---
+++ crash_course_book/chapter6/6_2_favorite_nmeber.py
@@ -0,0 +1,5 @@
+favorite_number = {'Rafael': '9', 'Otto': '14', 'Laura': '2', 'Julia': '14'}
+print(f"Rafael favorite number is {favorite_number['Rafael']} ")
+print(f"Laura favorite number is {favorite_number['Laura']} ")
+print(f"Otto favorite number is {favorite_number['Otto']} ")
+print(f"Ju favorite number is {favorite_number['Julia']} ")
---
+++ crash_course_book/chapter6/6_3_glossary.py
@@ -0,0 +1,12 @@
+glossary = {
+        'string': 'Any sequence of caracters',
+        'boolean': 'binary data type, yes/no, true/false',
+        'list': 'It is a list of values',
+        'integer': 'whole numbers',
+        'dictionaries': 'key value list'
+        }
+print(f"Boolean is,\n {glossary['boolean']}")
+print(f"String is, {glossary['string']}")
+print(f"List is, {glossary['list']}")
+print(f"Integer is, {glossary['integer']}")
+print(f"Dictionaries is,\n {glossary['dictionaries']}")
